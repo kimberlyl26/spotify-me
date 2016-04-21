28c6cf66f0e3260b502e3078976a11d086c79ad6
kimberlyl26 <kimberly.lanman@buchanan-edwards.com>
2016-04-21 18:27:54 -0400

    Spotify api hw

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,45 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 
+/*
+Enter a keyword to seach for.
+Select "artist" or "track" as the search type.
+Have the names of all search results print as a list.
+*/
+var $form = $('#search')
+var $searchType = $('#search-type');
+var $searchKey = $('#search-keyword');
+$(function (){
+    $form.on("submit", function(e){
+        e.preventDefault();
+        //Decide if searching by artist or track
+        var url;
+        var type = $searchType.val();
+        if(type == 'artist'){
+            url = searchByArtist($searchKey.val())
+        }
+        else{
+            url = searchByTrack($searchKey.val());
+        }
+        
+        $.ajax({
+            url: url,
+            method: "get",
+        }).done(function(response){
+            console.log(response)
+        })
+
+    })
+
+})
 
 function searchByArtist(keyword) {
-  var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+  var url = 'http://api.spotify.com/v1/search/?type=artist&q='+keyword;
+  return url;
 }
 
 
 function searchByTrack(keyword) {
-  var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
+  var url = 'http://api.spotify.com/v1/search/?type=track&q='+keyword;
+  return url;
 }
